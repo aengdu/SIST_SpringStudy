92a09ac1dfd1d4a45cc5cd319844c50f6b4efc30
sungmin <sungmin7113@gmail.com>
2023-03-08 12:09:17 +0900

    feat:foodsearch

--- SpringFinalProject/src/main/java/com/sist/aop/CommonAspect.java
+++ SpringFinalProject/src/main/java/com/sist/aop/CommonAspect.java
@@ -1,9 +1,16 @@
 package com.sist.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class CommonAspect {
+    @Around("execution(" *com.sist.web.*Controller.*(..))")
+    public Object around(ProceedingJoinPoint jp) throws Throwable {
+        Object obj = null;
+        return obj;
+    }
 }
